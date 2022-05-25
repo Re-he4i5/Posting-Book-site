a3b3f9b844bdb17a335537d316f9a1ce611e98da
Re-he4i5 <kuronekoryohei@gmail.com>
2022-05-25 14:37:33 +0900

    '[finish]bookers_1'

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'homes#top'
-  get 'home/about' => 'homes#about',as: 'about'
-  devise_for :users
-  resources :books, only: [:index, :show, :create, :edit, :destroy, :update] do
-    resource :favorites, only: [:create, :destroy]
-    resources :book_comments, only: [:create, :destroy]
-  end
-  resources :users, only: [:index, :show, :create, :edit, :update]
-  
-  #devise_forはなるべく上に記述
-  #:idによって別のルーティングが反映されること注意
-  
-  
-  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'homes#top'
+  resources :books
 end
